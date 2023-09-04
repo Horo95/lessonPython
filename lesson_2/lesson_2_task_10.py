26bb5e982f93e79bf2fd8a1cad8e8723c9e6a495
Horo95 <julborovikova95@gmail.com>
2023-09-04 20:51:54 +0300

    Домашнее задание 2

---
+++ lesson_2/lesson_2_task_10.py
@@ -0,0 +1,13 @@
+
+
+def bank(x,y):
+    for i in range(1,y+1):
+        p=x*0.1
+        x+=p
+        summa=x
+        print('Через', i, 'год/лет','на счету будет',summa)
+            
+x=int(input('Сумма первоначального вклада: '))
+y=int(input('Срок вклада(в годах): '))
+
+bank(x,y)
---
+++ lesson_2/lesson_2_task_9.py
@@ -0,0 +1,10 @@
+var_1 = 37
+var_2 = 99
+
+a=var_1
+b=var_2
+
+var_1=b
+var_2=a
+
+print(var_1,var_2)
