bc04d3a3c09517cc5fb3052e283c4cc62ea81bb3
Horo95 <julborovikova95@gmail.com>
2023-09-10 22:45:38 +0300

    Домашнее задание 3

--- lesson_3/mailing.py
+++ lesson_3/mailing.py
@@ -1,9 +1,5 @@
-from address import Address
-
 class Mailing:
     
-    
-
     def __init__(self, to_address, from_address, cost, track):
         cost = int(cost)
         track = str(track)
@@ -11,17 +7,3 @@ class Mailing:
         self.from_address = from_address
         self.cost = cost
         self.track = track
-
-
-        
-   
-
-
-
- 
-        
-
-
-        
-#45345,'Moscow','Pavlova',34,65
-#54785,'Moscow','Pnasenko',358,66
